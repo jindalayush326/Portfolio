bc856fedaf62a34ef7ea40927263745dd90137db
Ayush Jindal <104627503+jindalayush326@users.noreply.github.com>
2024-07-17 09:20:07 +0530

    Update particles.js

--- src/components/Particles/particles.js
+++ src/components/Particles/particles.js
@@ -1,25 +1,14 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-
-
+import { loadSlim } from "@tsparticles/slim"; // Make sure to install this package
 
 const ParticlesComponent = (props) => {
-
   const [init, setInit] = useState(false);
-  // this should be run only once per application lifetime
+
+  // This should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
@@ -29,14 +18,8 @@ const ParticlesComponent = (props) => {
     console.log(container);
   };
 
-
   const options = useMemo(
     () => ({
-      // background: {
-      //   color: {
-      //     value: "#1E2F97",
-      //   },
-      // },
       fpsLimit: 120,
       interactivity: {
         events: {
@@ -98,11 +81,10 @@ const ParticlesComponent = (props) => {
       },
       detectRetina: true,
     }),
-    [],
+    []
   );
 
-
-  return <Particles id={props.id} init={particlesLoaded} options={options} />; 
+  return <Particles id={props.id} init={particlesLoaded} options={options} />;
 };
 
 export default ParticlesComponent;
